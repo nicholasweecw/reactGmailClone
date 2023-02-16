87782ff95b06018269c40dff39a604effca4c096
Nicholas <nicholasweecw@gmail.com>
2023-02-16 10:18:42 +0800

    Fixed emailRow dateTimte & added animations for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import "../style/Header.css";
 import MenuIcon from "@mui/icons-material/Menu";
-import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import Navbar from "./Navbar";
 import picture from "../profile-pic.png";
 import { logout, selectUser } from "../features/userSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { Avatar } from "@mui/material";
+import { Avatar, IconButton } from "@mui/material";
 import { auth } from "../firebase";
 
 const Header = () => {
@@ -25,7 +25,9 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header__left">
-        <MenuIcon className="menu-icon" />
+        <IconButton className="btn">
+          <MenuIcon className="menu-icon" />
+        </IconButton>
         <img
           src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r5.png"
           alt=""
@@ -36,7 +38,15 @@ const Header = () => {
       <Navbar />
 
       <div className="header__right">
-        <SettingsOutlinedIcon className="icon" />
+        <IconButton className="btn">
+          <HelpOutlineOutlinedIcon />
+        </IconButton>
+        <IconButton className="btn">
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton className="btn">
+          <AppsOutlinedIcon />
+        </IconButton>
         <Avatar
           onClick={signOut}
           src={user?.photoUrl}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import SearchIcon from "@mui/icons-material/Search";
+import { IconButton } from "@mui/material";
 import "../style/Navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <form className="form">
-        <button className="btn">
+        <IconButton className="btn">
           <SearchIcon className="search-icon" />
-        </button>
+        </IconButton>
         <input className="input" placeholder="Search mail" type="text"></input>
       </form>
     </div>
